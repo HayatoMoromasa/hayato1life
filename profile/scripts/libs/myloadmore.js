a3e0949793b7fb683dc8ebf5516330cb5a8abc29
HayatoMoromasa <50811729+HayatoMoromasa@users.noreply.github.com>
2020-08-21 22:10:33 +0900

    Update myloadmore.js

--- profile/scripts/libs/myloadmore.js
+++ profile/scripts/libs/myloadmore.js
@@ -1,33 +1,31 @@
-jQuery(function($){ // use jQuery code inside this to avoid "$ is not defined" error
+jQuery(function($){ 
 	$('.ajax_loadmore').click(function(){
  
 		var button = $(this),
 		post_type = button.data('pt'),
 		    data = {
 			'action': 'loadmore',
-			'query': ajax_loadmore_params.posts, // that's how we get params from wp_localize_script() function
+			'query': ajax_loadmore_params.posts, 
 			'page' : ajax_loadmore_params.current_page,
 			'post_type':	post_type ? post_type : 'post'
 		};
  
-		$.ajax({ // you can also use $.post here
-			url : ajax_loadmore_params.ajaxurl, // AJAX handler
+		$.ajax({ 
+			url : ajax_loadmore_params.ajaxurl, 
 			data : data,
 			type : 'POST',
 			beforeSend : function ( xhr ) {
-				button.text('Loading.....'); // change the button text, you can also add a preloader image
+				button.text('Loading.....'); 
 			},
 			success : function( data ){
 				if( data ) { 
-					button.text( 'More posts' ).prev().after(data); // insert new posts
+					button.text( 'More posts' ).prev().after(data); 
 					ajax_loadmore_params.current_page++;
 					if ( ajax_loadmore_params.current_page == ajax_loadmore_params.max_page ) 
-						button.remove(); // if last page, remove the button
- 
-					// you can also fire the "post-load" event here if you use a plugin that requires it
-					// $( document.body ).trigger( 'post-load' );
+						button.remove(); 
+					
 				} else {
-					button.remove(); // if no data, remove the button as well
+					button.remove(); 
 				}
 			}
 		});
